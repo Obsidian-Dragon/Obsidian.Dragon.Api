6c574d9e902a59c18f15ac7ec4e3cc2d2084a964
Oladapo Olobatuyi <opolotuyi@gmail.com>
2022-04-30 02:44:47 -0400

    test2

--- Obsidian.Dragon.Api/Program.cs
+++ Obsidian.Dragon.Api/Program.cs
@@ -13,6 +13,8 @@ string authority = builder.Configuration["Auth0:Authority"] ??
 string audience = builder.Configuration["Auth0:Audience"] ??
     throw new ArgumentNullException("Auth0:Audience");
 
+string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
+    throw new ArgumentNullException("ConnectionString:StoreConnection");
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -35,7 +37,7 @@ builder.Services.AddAuthorization(options =>
     });
 
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite",
+    options.UseSqlServer(storeConnectionString,
     b => b.MigrationsAssembly("Obsidian.Dragon.Api"))
 );
 
